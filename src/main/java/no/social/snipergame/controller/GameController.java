3810e23a48b440c30aac9d09ac939c41ec451c77
Christian <defrenechr@me.com>
2016-04-18 13:42:43 +0200

    MArk should now mark the center of the tile

--- src/main/java/no/social/snipergame/controller/GameController.java
+++ src/main/java/no/social/snipergame/controller/GameController.java
@@ -1,6 +1,7 @@
 package no.social.snipergame.controller;
 
 import com.google.gson.Gson;
+
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,12 +11,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
+import javafx.scene.effect.InnerShadow;
 import no.social.snipergame.model.Coordinates;
 import no.social.snipergame.model.Game;
 import no.social.snipergame.network.Client;
@@ -57,7 +60,7 @@ public class GameController implements Initializable  {
 
     private Coordinates markedCoordinates, curserCoordinates;
 
-    private boolean sniper = false;
+    private boolean sniper = true;
 
     private String name = sniper ? "Sniper" : "Spotter";
     private NetworkConnection connection = sniper ? createServer() : createClient();
@@ -70,6 +73,7 @@ public class GameController implements Initializable  {
         // Select a random tile from the list
         String[] tiles = new String[]{"brick", "desert", "grass", "ground", "ground2"};
         String tile = tiles[(int) (Math.random() * 5)];
+        // Game window: X:24, Y:17
         for (int i = 0; i < 24*17; i++) {
             StackPane stackPane = new StackPane(new ImageView("/icons/" + tile + ".png"));
             if (game.getPersons()[i] != null) stackPane.getChildren().add(game.getPersons()[i]);
@@ -79,12 +83,15 @@ public class GameController implements Initializable  {
         nameLabel.setText(name);
         nameLabel.setTextFill(sniper ? Color.RED : Color.GREEN);
         coordinateLabel.setText(markedCoordinates.toString());
+        
+        
         if (sniper) {
             connection = createServer();
             windLabel.setText("");
             coordinateLabel.setText("");
             sendCoordinatesButton.setVisible(false);
             sendWindButton.setVisible(false);
+            background.setEffect(new ColorAdjust(0, -1, 0, 0));
         }
 
         background.setCursor(new ImageCursor(scope, scope.getWidth()/2, scope.getHeight()/2));
@@ -99,8 +106,10 @@ public class GameController implements Initializable  {
             Dimension2D dim = ImageCursor.getBestSize(event.getScreenX(), event.getScreenY());
             mark.setFitHeight(dim.getHeight());
             mark.setFitWidth(dim.getWidth());
-            mark.setLayoutX(event.getX() - mark.getFitWidth() / 2);
-            mark.setLayoutY(event.getSceneY() - (1.65* mark.getFitHeight()));
+            double X = event.getX() - mark.getFitWidth() / 2;
+            double Y = event.getSceneY() - (1.65* mark.getFitHeight());
+            mark.setLayoutX((int)X + 20);
+            mark.setLayoutY((int)Y + 20);
             background.getChildren().add(mark);
         });
         try {
